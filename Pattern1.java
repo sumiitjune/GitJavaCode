6e4148b85994f0f8540eda8e5ed2917bea04eab3
sumitkr <sumiitjune@gmail.com>
2026-03-14 21:00:36 +0000

    Pattern_Program

---
+++ Pattern1.java
@@ -0,0 +1,14 @@
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+
+        for (int i = 1; i <=  num1; i++) {
+            for (int j = 1; j <= num1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
